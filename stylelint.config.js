025cb9e32e2ba727207225bd29b0072ab3e1efb6
yehongrun <yehongrun@yhyl.com>
2021-05-19 09:43:24 +0800

    style: 新增 stylelint 支持 ::v-deep 样式穿透

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,5 +2,12 @@ module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-order'],
   rules: {
     'no-empty-source': null,
+    // 支持 v-deep
+    'selector-pseudo-element-no-unknown': [
+      true,
+      {
+        ignorePseudoElements: ['v-deep'],
+      },
+    ],
   },
 };
